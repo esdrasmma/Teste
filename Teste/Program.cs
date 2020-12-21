70c997d5d7008024329e937078147eb33f0689da
Marcelo Esdras <esdrasmma@gmail.com>
2020-12-21 13:41:02 -0300

    Acrescentado a mensagem Bom dia

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -7,6 +7,11 @@ namespace Teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
+
+            Console.ReadLine();
+
+
         }
     }
 }
